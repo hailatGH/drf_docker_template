a34c20a22cd8fbcf9c98f0c6eee4b066eaa92293
hailatGH <hailat.alx@gmail.com>
2022-12-14 09:39:48 +0300

    disallowed hosts fixed

--- src/core/settings.py
+++ src/core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-qj#wbv&!_b3c6v@4uc-qf#ujvu9v*!(ar0-11^4!@t(32^m*69
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+
     'rest_framework',
 
     'core',
