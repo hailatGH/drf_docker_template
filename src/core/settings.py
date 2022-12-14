8960cbf245cd75b12826a3ed0659349cea1d7c10
hailatGH <hailat.alx@gmail.com>
2022-12-14 16:26:04 +0300

    ereeee wooo

--- src/core/settings.py
+++ src/core/settings.py
@@ -190,13 +190,13 @@ STATICFILES_STORAGE = 'storages.backends.azure_storage.AzureStorage'
 
 AZURE_ACCOUNT_NAME = 'zemastroragev100'
 AZURE_ACCOUNT_KEY = 'AFsY2hZVbyYBKisEkRL+toNNJ7yBOzoJ/cruOxurFHnU84vE+Cmloq9S2ZkCxYaxrM5QemPsUiX5+ASt4WEg8w=='
+AZURE_URL_EXPIRATION_SECS = None
 AZURE_CONTAINER = 'zemacontainer'
 AZURE_SSL = False
-STATIC_LOCATION = "zemacontainer"
 AZURE_CUSTOM_DOMAIN = f'{AZURE_ACCOUNT_NAME}.blob.core.windows.net'
+
+STATIC_LOCATION = "zemacontainer"
 STATIC_URL = f'https://{AZURE_CUSTOM_DOMAIN}/{STATIC_LOCATION}/'
-AZURE_URL_EXPIRATION_SECS = None
-# STATICFILES_STORAGE = 'core.custom_azure.AzureStaticStorage'
-STATIC_ROOT = os.path.join(BASE_DIR, f'{STATIC_LOCATION}/')
+STATIC_ROOT = f'https://{AZURE_CUSTOM_DOMAIN}/{STATIC_LOCATION}/'
 
 AZURE_CONNECTION_STRING = "DefaultEndpointsProtocol=https;AccountName=zemastroragev100;AccountKey=AFsY2hZVbyYBKisEkRL+toNNJ7yBOzoJ/cruOxurFHnU84vE+Cmloq9S2ZkCxYaxrM5QemPsUiX5+ASt4WEg8w==;BlobEndpoint=https://zemastroragev100.blob.core.windows.net/;FileEndpoint=https://zemastroragev100.file.core.windows.net/;TableEndpoint=https://zemastroragev100.table.core.windows.net/;QueueEndpoint=https://zemastroragev100.queue.core.windows.net/"
