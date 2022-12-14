b00b806f03305fc19ac115baa43e2ec35cb1d44a
hailatGH <hailat.alx@gmail.com>
2022-12-14 09:57:43 +0300

    database connection added

--- src/core/settings.py
+++ src/core/settings.py
@@ -79,13 +79,13 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+DATABASES = {'default': 'postgres://zemadatabaseadmin:StrongP@ssword@zema-postgresql-v100.postgres.database.azure.com/zema-postgresql-v100'}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
