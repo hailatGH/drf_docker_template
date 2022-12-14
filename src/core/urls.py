c3b71f9a762e977f8b60772e4d58ffa4960d7137
hailatGH <hailat.alx@gmail.com>
2022-12-14 14:49:38 +0300

    storage test

--- src/core/urls.py
+++ src/core/urls.py
@@ -24,7 +24,7 @@ router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'users', UserViewSet)
 router.register(r'groups', GroupViewSet)
 router.register(r'test', TestViewset)
-router.register(r'test2', Test2Viewset)
+router.register(r'test7', Test2Viewset)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
