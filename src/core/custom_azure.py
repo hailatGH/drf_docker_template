07d62c32c84444b8a42bcdf1e184e1d6c9a697e8
hailatGH <hailat.alx@gmail.com>
2022-12-14 17:08:29 +0300

    hope

--- src/core/custom_azure.py
+++ src/core/custom_azure.py
@@ -1,12 +1,12 @@
 from storages.backends.azure_storage import AzureStorage
 
 
-class AzureStaticStorage(AzureStorage):
-    account_name = 'zemastroragev100'  # Must be replaced by your storage_account_name
-    # Must be replaced by your <storage_account_key>
-    account_key = 'AFsY2hZVbyYBKisEkRL+toNNJ7yBOzoJ/cruOxurFHnU84vE+Cmloq9S2ZkCxYaxrM5QemPsUiX5+ASt4WEg8w=='
-    azure_container = 'zemacontainer'
-    expiration_secs = None
+# class AzureStaticStorage(AzureStorage):
+#     account_name = 'zemastroragev100'  # Must be replaced by your storage_account_name
+#     # Must be replaced by your <storage_account_key>
+#     account_key = 'AFsY2hZVbyYBKisEkRL+toNNJ7yBOzoJ/cruOxurFHnU84vE+Cmloq9S2ZkCxYaxrM5QemPsUiX5+ASt4WEg8w=='
+#     azure_container = 'zemacontainer'
+#     expiration_secs = None
 
 
 class AzureMediaStorage(AzureStorage):
