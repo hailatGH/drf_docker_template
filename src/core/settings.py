efc85c2d132298b2e4d02fff253726ee91f85b1f
hailatGH <hailat.alx@gmail.com>
2022-12-14 11:39:00 +0300

    database connection set

--- src/core/settings.py
+++ src/core/settings.py
@@ -79,12 +79,25 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': "zemadatabase",
+        'USER': "zemadatabaseadmin",
+        'PASSWORD': "StrongP@ssword",
+        'HOST': "zema-postgresql-v100.postgres.database.azure.com",
+        'PORT': '5432',
+        'OPTIONS': {'sslmode': 'disable'}
     }
 }
+
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql',
